c2ac87f6f78f06f6f30e65f046bb29768209368a
JaimeA404 <jaimerafaavan@gmail.com>
2025-10-18 18:53:19 -0400

    game work

--- Game.cs
+++ Game.cs
@@ -1,4 +1,4 @@
-﻿// Include the namespaces (code libraries) you need below.
+// Include the namespaces (code libraries) you need below.
 using Raylib_cs;
 using System;
 using System.Numerics;
@@ -11,18 +11,28 @@ namespace MohawkGame2D
     /// </summary>
     public class Game
     {
-        // Place your variables here:
+        // Place your variables here
 
+        // damage tracking + display
 
         int punches;
         int slaps;
         float totalDamage;
         float punchPower = 0f;
         float chargeStart = 0f;
-        bool isChargingPunch = false;
         float slapPower = 0f;
-        bool isChargingSlap = false;
 
+        // dialogue system
+
+        float messageStartTime = -1f;
+        float messageActivity = 2f; //forces the message to show for 2 seconds
+        bool messageActive25 = false;
+        bool messageShown25 = false;
+        bool messageActive50 = false;
+        bool messageShown50 = false;
+        bool messageActive75 = false;
+        bool messageShown75 = false;
+        bool guyBeatToDeath = false;
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
@@ -46,7 +56,7 @@ namespace MohawkGame2D
             {
                 chargeStart = Time.SecondsElapsed;
             }
-            // determines of the user is charging a punch
+            // determines of the user is charging a punch or tapping the left mouse button
             if (Input.IsMouseButtonDown(MouseInput.Left))
             {
                 float heldTime = Time.SecondsElapsed - chargeStart;
@@ -55,7 +65,7 @@ namespace MohawkGame2D
                 punchPower = (heldTime) * 2;
                 if (heldTime <= 1f) punchPower = 1;
             }
-            
+
             if (Input.IsMouseButtonReleased(MouseInput.Left))
             {
                 punches++;
@@ -64,7 +74,7 @@ namespace MohawkGame2D
                 int x = (int)Input.GetMouseX();
                 int y = (int)Input.GetMouseY();
 
-                int radius = (int)(punchPower * 10);
+                int radius = (int)(punchPower * 30);
 
                 Draw.FillColor = Color.Red;
                 Draw.Circle(x, y, radius);
@@ -75,8 +85,8 @@ namespace MohawkGame2D
             {
                 chargeStart = Time.SecondsElapsed;
             }
-            // determines of the user is charging a slap
-            if (Input.IsMouseButtonDown (MouseInput.Right))
+            // determines of the user is charging a slap or tapping the right mouse button
+            if (Input.IsMouseButtonDown(MouseInput.Right))
             {
                 float heldTime = Time.SecondsElapsed - chargeStart;
                 if (heldTime > 5f) heldTime = 5f;
@@ -90,15 +100,100 @@ namespace MohawkGame2D
                 slaps++;
                 totalDamage += slapPower;
 
+                int x = (int)Input.GetMouseX();
+                int y = (int)Input.GetMouseY();
 
+                int width = (int)(slapPower * 20);
+                int height = (int)(slapPower * 60);
+
+                Draw.FillColor = Color.Red;
+                Draw.Rectangle(x - width / 2, y - height / 2, width, height);
             }
-            
+
+            //HUD displays information
+
             Raylib.DrawText("Punch with Left Mouse Button", 10, 10, 20, Color.Black);
             Raylib.DrawText("Slap with Right Mouse Button", 10, 40, 20, Color.Black);
             Raylib.DrawText("Punches: " + punches, 10, 70, 20, Color.Black);
             Raylib.DrawText("Slaps: " + slaps, 10, 100, 20, Color.Black);
             Raylib.DrawText("Total Damage: " + (int)totalDamage, 10, 130, 20, Color.Black);
+
+            // first message at >=25 damage
+
+            if (totalDamage >= 25f && totalDamage < 50 && !messageShown25)
+            {
+                messageShown25 = true;
+                messageActive25 = true;
+                messageStartTime = Time.SecondsElapsed;
+            }
+            if (messageActive25)
+            {
+                if (Time.SecondsElapsed - messageStartTime < messageActivity)
+                {
+                    Raylib.DrawText("Please... I have a family...", 500, 10, 20, Color.Black);
+
+                    if (Time.SecondsElapsed == 2f)
+                    {
+                        messageActive25 = false;
+                        messageShown25 = false;
+                    }
+                }
+            }
+
+            // second message at >=50 damage
+
+            if (totalDamage >= 50 && totalDamage < 75 && !messageShown50)
+            {
+                messageActive25 = false;
+                messageShown25 = false;
+                messageShown50 = true;
+                messageActive50 = true;
+                messageStartTime = Time.SecondsElapsed;
+            }
+            if (messageActive50)
+            {
+                if (Time.SecondsElapsed - messageStartTime < messageActivity)
+                {
+                    Raylib.DrawText("STOP!", 550, 10, 20, Color.Black);
+                    Raylib.DrawText("WHAT DID I EVER DO TO YOU?!", 450, 40, 20, Color.Black);
+                    if (Time.SecondsElapsed == 2f)
+                    {
+                        messageActive50 = false;
+                        messageShown50 = false;
+                    }
+                }
+            }
+
+            if (totalDamage >= 75f && totalDamage < 100 && !messageShown75)
+            {
+                messageActive50 = false;
+                messageShown50 = false;
+                messageShown75 = true;
+                messageActive75 = true;
+                messageStartTime = Time.SecondsElapsed;
+            }
+            if (messageActive75)
+            {
+                if (Time.SecondsElapsed - messageStartTime < messageActivity)
+                {
+                    Raylib.DrawText("Make it stop...", 500, 10, 20, Color.Black);
+                    if (Time.SecondsElapsed == 2f)
+                    {
+                        messageActive75 = false;
+                        messageShown75 = false;
+                    }
+                }
+            }
+
+            if (totalDamage >= 100f)
+            {
+                guyBeatToDeath = true;
+            }
+            if (guyBeatToDeath)
+            {
+                Raylib.ClearBackground(Color.White);
+                Raylib.DrawText("You have beaten the guy to death!", 250, 300, 30, Color.Black);
+            }
         }
     }
-
 }
